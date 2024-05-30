c94912be607df4ab62c3959d3ff26b3394e346f6
dmitruz <dmitruz970@gmail.com>
2024-05-31 00:50:29 +0300

    Add Tailwind css

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+
+
+export const Home: React.FC = () => {
+    return (
+        <div className="flex flex-col items-center">
+            <h1>Garage</h1>
+            <p>Let's enjoy this awesome app!🤸‍♂️</p>
+        </div>
+    );
+}
